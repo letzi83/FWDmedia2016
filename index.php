f0b6eb0f49058c19b57ada89f077349f4b8118b7
MiczFlor <micz.flor@web.de>
2016-02-08 10:03:40 +0100

    Initial commit with license and theme credits

---
+++ index.php
@@ -0,0 +1,311 @@
+<?php
+include("lang-en.php");
+?>
+<!DOCTYPE HTML>
+<!--
+	Spectral by HTML5 UP
+	html5up.net | @n33co
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title><FWD:media></title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+		<!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
+	</head>
+	<body class="landing">
+
+		<!-- Page Wrapper -->
+			<div id="page-wrapper">
+
+				<!-- Header -->
+					<header id="header" class="alt">
+						<h1><a href="#top">&lt;FWD:media&gt;</a></h1>
+						<nav id="nav">
+							<ul>
+								<li class="special">
+								  <li><a href="#"><?php echo $lang['lang-en']; ?></a></li>
+								  <li><a href="#"><?php echo $lang['lang-fr']; ?></a></li>
+								  <li><a href="#"><?php echo $lang['lang-es']; ?></a></li>
+								</li>
+								<li class="special">
+									<a href="#menu" class="menuToggle"><span><?php echo $lang['nav-menu']; ?></span></a>
+									<div id="menu">
+										<ul>
+											<li><a href="#"><?php echo $lang['nav-home']; ?></a></li>
+											<li><a href="#start" class="scrolly"><?php echo $lang['nav-start']; ?></a></li>
+											<li><a href="#whatislb" class="scrolly"><?php echo $lang['nav-lbabout']; ?></a></li>
+											<li><a href="#weprovide" class="scrolly"><?php echo $lang['nav-what']; ?></a></li>
+											<li><a href="#whocanapply"><?php echo $lang['nav-who']; ?></a></li>
+											<li><a href="#apply"><?php echo $lang['nav-apply']; ?></a></li>
+											<li><a href="#faq"><?php echo $lang['nav-faq']; ?></a></li>
+											<li><a href="#contact"><?php echo $lang['nav-contact']; ?></a></li>
+										</ul>
+									</div>
+								</li>
+							</ul>
+						</nav>
+					</header>
+
+				<!-- Banner -->
+					<section id="banner">
+						<div class="inner">
+							<h2><?php echo $lang['head-title']; ?></h2>
+							<p><?php echo $lang['head-claim']; ?></p>
+							<ul class="actions">
+								<li><a href="#" class="button special"><?php echo $lang['btn-applynow']; ?></a></li>
+							</ul>
+						</div>
+						<a href="#start" class="more scrolly"><?php echo $lang['btn-more']; ?></a>
+					</section>
+
+				<!-- One -->
+					<section id="start" class="wrapper style1 special">
+						<div class="inner">
+							<header class="major">
+								<h2><?php echo $lang['sourcefabricsinitiative']; ?></h2>
+								<p>
+								  <?php echo $lang['rumediaorg']; ?>
+								  <a href='https://www.sourcefabric.org/en/liveblog/' target='_blank'><?php echo $lang['btn-checklb']; ?></a>
+								</p>
+								<h2><?php echo $lang['deadlinetxt']; ?></h2>
+							</header>
+						</div>
+					</section>
+
+				<!-- Learn about live blogging -->
+					<section id="whatislb" class="lb-bg-banner bigtxt ">
+						<div class="inner">
+							<header class="major">
+								<h2><?php echo $lang['biglb4fm']; ?></h2>
+								<p><?php echo $lang['biglb4fminfo']; ?></p>
+								<!--p>
+    						  <a href="#whatislbspotlight" class="smalltxt more scrolly"><?php echo $lang['btn-more']; ?></a>
+								</p-->
+							</header>
+						</div>
+					</section>
+
+				<!-- Live blog details -->
+					<section id="whatislbspotlight" class="wrapper alt style2">
+						<section class="spotlight">
+							<div class="image"><img src="images/lb-icon-01.png" alt="" /></div><div class="content">
+								<h2 class=""><?php echo $lang['spotlight-a']; ?></h2>
+							</div>
+						</section>
+						<section class="spotlight">
+							<div class="image"><img src="images/lb-icon-02.png" alt="" /></div><div class="content">
+								<h2 class=""><?php echo $lang['spotlight-b']; ?></h2>
+							</div>
+						</section>
+						<section class="spotlight">
+							<div class="image"><img src="images/lb-icon-03.png" alt="" /></div><div class="content">
+								<h2 class=""><?php echo $lang['spotlight-c']; ?></h2>
+							</div>
+						</section>
+					</section>
+
+				<!-- we provide -->
+					<section id="weprovide" class="wrapper style3 special">
+						<div class="inner">
+							<header class="major">
+								<h2 class="upperc"><?php echo $lang['weprovide']; ?></h2>
+								<p><?php echo $lang['weprovideinfo']; ?></p>
+							</header>
+							<ul class="features">
+								<li class="icon fa-paper-plane-o">
+									<h3><?php echo $lang['weprovide-title-a']; ?></h3>
+									<p><?php echo $lang['weprovide-info-a']; ?></p>
+								</li>
+								<li class="icon fa-laptop">
+									<h3><?php echo $lang['weprovide-title-b']; ?></h3>
+									<p><?php echo $lang['weprovide-info-b']; ?></p>
+								</li>
+								<li class="icon fa-code">
+									<h3><?php echo $lang['weprovide-title-c']; ?></h3>
+									<p><?php echo $lang['weprovide-info-c']; ?></p>
+								</li>
+								<li class="icon fa-users">
+									<h3><?php echo $lang['weprovide-title-d']; ?></h3>
+									<p><?php echo $lang['weprovide-info-d']; ?></p>
+								</li>
+								<li class="icon fa-heart-o">
+									<h3><?php echo $lang['weprovide-title-e']; ?></h3>
+									<p><?php echo $lang['weprovide-info-e']; ?></p>
+								</li>
+								<li class="icon fa-lightbulb-o">
+									<h3><?php echo $lang['weprovide-title-f']; ?></h3>
+									<p><?php echo $lang['weprovide-info-f']; ?></p>
+								</li>
+							</ul>
+						</div>
+					</section>
+
+				<!-- what we do not provide -->
+					<section id="notprovide" class="wrapper style2">
+						<div class="inner narrow">
+							<header>
+								<h2><?php echo $lang['notprovide']; ?></h2>
+											<ul class="">
+												<li><?php echo $lang['notprovide-a']; ?></li>
+												<li><?php echo $lang['notprovide-b']; ?></li>
+												<li><?php echo $lang['notprovide-c']; ?></li>
+												<li><?php echo $lang['notprovide-d']; ?></li>
+												<li><?php echo $lang['notprovide-e']; ?></li>
+											</ul>
+								<p><?php echo $lang['notprovideinfo']; ?></p>
+							</header>
+						</div>
+					</section>
+
+				<!-- who should apply -->
+					<section id="whoapply" class="wrapper style5">
+						<div class="inner narrow">
+								<h2><span class="icon fa-check"></span> <?php echo $lang['whoapply']; ?></h2>
+								<p><?php echo $lang['whoapplyinfo']; ?></p>
+
+									<div class="table-wrapper">
+										<table>
+											<tbody>
+												<tr>
+													<td><span class="icon fa-check"></span></td>
+													<td><?php echo $lang['whoapply-a']; ?></td>
+												</tr>
+												<tr>
+													<td><span class="icon fa-check"></span></td>
+													<td><?php echo $lang['whoapply-b']; ?></td>
+												</tr>
+												<tr>
+													<td><span class="icon fa-check"></span></td>
+													<td><?php echo $lang['whoapply-c']; ?></td>
+												</tr>
+												<tr>
+													<td><span class="icon fa-check"></span></td>
+													<td><?php echo $lang['whoapply-d']; ?></td>
+												</tr>
+												<tr>
+													<td><span class="icon fa-check"></span></td>
+													<td><?php echo $lang['whoapply-e']; ?></td>
+												</tr>
+										</table>
+									</div>
+								
+								<p><?php echo $lang['whoapplybottominfo']; ?></p>
+						</div>
+					</section>
+					
+				<!-- contact form-->
+					<section id="applynow" class="wrapper style3">
+						<div class="inner">
+  						<h2><?php echo $lang['applynow']; ?></h2>
+  						<p><?php echo $lang['applynowinfo']; ?></p>
+  						<form method="post" action="#">
+  							<div class="row uniform">
+  								<div class="6u 12u$(xsmall)">
+  									<input type="text" name="demo-name" id="demo-name" value="" placeholder="Name" />
+  								</div>
+  								<div class="6u$ 12u$(xsmall)">
+  									<input type="email" name="demo-email" id="demo-email" value="" placeholder="Email" />
+  								</div>
+  								<div class="12u$">
+  									<textarea name="demo-message" id="demo-message" placeholder="Enter your message" rows="6"></textarea>
+  								</div>
+  								<div class="12u$">
+  									<ul class="actions">
+  										<li><input type="submit" value="Send Message" class="special" /></li>
+  										<li><input type="reset" value="Reset" /></li>
+  									</ul>
+  								</div>
+  							</div>
+  						</form>
+  				  </div>
+					</section>
+
+				<!-- FAQ -->
+					<section id="faq" class="wrapper style5">
+						<div class="inner">
+								<h2><?php echo $lang['faq']; ?></h2>
+								<p><?php echo $lang['faqinfo']; ?></p>
+								<ul class="alt" id="drawers">
+                 <li>
+                    <h4><?php echo $lang['faq-a-q']; ?></h4>
+                    <div style="display: none;"><?php echo $lang['faq-a-a']; ?></div>
+                 </li>
+                 <li>
+                    <h4><?php echo $lang['faq-b-q']; ?></h4>
+                    <div style="display: none;"><?php echo $lang['faq-b-a']; ?></div>
+                 </li>
+                 <li>
+                    <h4><?php echo $lang['faq-c-q']; ?></h4>
+                    <div style="display: none;"><?php echo $lang['faq-c-a']; ?></div>
+                 </li>
+              </ul>
+						</div>
+					</section>
+					
+
+				<!-- contactusinfo -->
+					<section id="contactfooter" class="wrapper style2 special">
+						<div class="inner">
+							<header class="major">
+								<h2 class="upperc"><?php echo $lang['bottomcontact']; ?></h2>
+								<p><?php echo $lang['bottomcontactinfo']; ?></p>
+							</header>
+							<ul class="features">
+								<li class="icon fa-binoculars">
+									<h3><?php echo $lang['bottomcontact-title-a']; ?></h3>
+									<p><?php echo $lang['bottomcontact-info-a']; ?></p>
+								</li>
+								<li class="icon  fa-envelope-o">
+									<h3><?php echo $lang['bottomcontact-title-b']; ?></h3>
+									<p><?php echo $lang['bottomcontact-info-b']; ?></p>
+								</li>
+							</ul>
+						</div>
+					</section>
+					
+				<!-- Footer -->
+					<footer id="footer">
+						<ul class="icons">
+							<li><a href="http://www.facebook.com/Sourcefabric" target="_blank" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
+							<li><a href="http://twitter.com/sourcefabric" target="_blank" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
+							<li><a href="https://plus.google.com/102556733644257731505" target="_blank" class="icon fa-google-plus"><span class="label">Google+</span></a></li>
+							<li><a href="http://www.flickr.com/photos/sourcefabric" target="_blank" class="icon fa-flickr"><span class="label">Flickr</span></a></li>
+							<li><a href="http://vimeo.com/sourcefabric" target="_blank" class="icon fa-vimeo"><span class="label">Vimeo</span></a></li>
+							<li><a href="https://www.linkedin.com/company/sourcefabric" target="_blank" class="icon fa-linkedin"><span class="label">LinkedIn</span></a></li>
+							<li><a href="mailto:contact@sourcefabric.org" class="icon fa-envelope-o"><span class="label">Email</span></a></li>
+							<li><a href="https://www.sourcefabric.org/en/home/rss" target="_blank" class="icon fa-rss"><span class="label">RSS</span></a></li>
+						</ul>
+						<ul class="copyright">
+							<li>
+							&copy; 2016 
+							<a href='https://www.sourcefabric.org/' target='_blank'>Sourcefabric z.ú.</a> 
+							This work is licensed under a 
+							<a href='https://creativecommons.org/licenses/by/3.0/' target='_blank'>Creative Commons Attribution 3.0 Unported License</a>
+							</li>
+						</ul>
+					</footer>
+
+			</div>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.scrollex.min.js"></script>
+			<script src="assets/js/jquery.scrolly.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+			<script src="assets/js/main.js"></script>
+<script type="text/javascript">
+  $('#drawers').find('h4').click(function(){
+    $(this).next().slideToggle();
+    $("#drawers div").not($(this).next()).slideUp();
+  });
+</script>
+
+	</body>
+</html>
---
+++ lang-en.php
@@ -0,0 +1,167 @@
+<?php
+$lang = array(
+
+  "sourcefabricsinitiative" => "Sourcefabric's &lt;FWD:media&gt; initiative provides<br/>
+    free live blogging services and support to a<br/>
+    selected number of independent web projects in<br/>
+    journalism and activism.",
+  "rumediaorg" => "Are you a media organisation, association or NGO working to integrate technology 
+    into a journalism or reporting project and looking for a live storytelling tool? 
+    We can help.",
+  "deadlinetxt" => "The first round of applications deadline is 31 March 2016.",
+  "biglb4fm" => "Live Blog for <br/> &lt;FWD:media&gt;",
+  "biglb4fminfo" => "Live Blog has been developed to serve <br>
+    media organisations reporting on crisis or real-time situations <br>
+    like natural disasters or breaking news.",
+  "spotlight-a" => "Live Blog is made for journalists, reporters and organisations to tell
+    their story. Whether from your desk or from the ground, you can
+    update your live blog in real-time.",
+  "spotlight-b" => "Live Blog is easily embedded in your website. Simply paste the
+    code and start spreading the news!",
+  "spotlight-c" => "Live Blog is easy to use and is web-based (no need to install
+    software). It is versatile, flexible and it can incorporate
+    text, images, social media, videos, audio, etc.",
+    
+  "weprovide" => "What we provide",
+  "weprovideinfo" => "Here's what you can expect when you partner up with us to use Live Blog",
+  "weprovide-title-a" => "We get you started",
+  "weprovide-info-a" => "You need no coding skills to start and run a
+    Live Blog <FWD:media> project. We set you
+    up with a short personal video training. We
+    have a number of fun tutorials and we offer
+    you basic support throughout your project.",
+  "weprovide-title-b" => "We host your Live Blog",
+  "weprovide-info-b" => "Live Blog is hosted in the cloud, managed
+    (and paid for) by Sourcefabric. Generally, we
+    sign you on for six months' hosting. If your
+    project is longer, don't worry. Tell us about it
+    and we will see what we can do.",
+  "weprovide-title-c" => "We give you training",
+  "weprovide-info-c" => "With the &lt;FWD:media&gt; initiative,
+    Sourcefabric is offering a basic support
+    package. This means a 2-4 hours start up
+    training, including basic live reporting
+    coaching, if necessary.",
+  "weprovide-title-d" => "We follow you throughout",
+  "weprovide-info-d" => "Live Blog can be embedded in any website
+    easily. We will guide you throughout your
+    project. We breathe journalism, we have
+    experience in supporting live reporting
+    worldwide.",
+  "weprovide-title-e" => "We make you part of our community",
+  "weprovide-info-e" => "We have partners all over the world and
+    they have advice and experience that can
+    benefit all of us. We encourage discussion
+    and mutual support and continuously refine
+    our software. We love to learn
+    improvement ideas from all members of the
+    community.",
+  "weprovide-title-f" => "We help you shine",
+  "weprovide-info-f" => "We share with you ideas for increased
+    visibility and outreach of your project. We
+    can help you develop your communication
+    strategy and we share our networks for
+    outreach.",
+    
+  "notprovide" => "What we do not offer <br/> within &lt;FWD:media&gt; *",
+  "notprovideinfo" => "* If you are in need of any of the above, feel free to ask us! We might be able to advise you.",
+  "notprovide-a" => "Financial support or grants",
+  "notprovide-b" => "Travel expenses",
+  "notprovide-c" => "Tech equipment",
+  "notprovide-d" => "New software features",
+  "notprovide-e" => "Website redesign",
+  
+  "whoapply" => "Who should apply",
+  "whoapplyinfo" => "We want to work with you, but sometimes, that's not enough. Here's a
+    checklist to see if we are a good match.",
+  "whoapply-a" => "Do you have a journalistic or nonprofit project needing a
+    new collaborative storytelling tool to report live events
+    from the ground? Say yes! ;) No kidding, we’d like to see
+    that you’ve thought about whether there’s a genuine
+    match between Live Blog and your project. And have a
+    solid argument for it.",
+  "whoapply-b" => "Is your project linked to independent media
+    organisations based in the Global South, Eastern Europe
+    and/or run by minority groups? We’d like to focus our
+    efforts. If you aren't based in these areas, or are working
+    on another type of social change project, please get in
+    touch with us directly to talk more about possible
+    avenues of support.",
+  "whoapply-c" => "How long has your project or organisation been running?
+    It's not always a problem if you're brand new, but it helps
+    if we can see some proof that you are sustainable.",
+  "whoapply-d" => "You are committed to actively use Live Blog for your
+    project and you have already allocated human and
+    technical resources to produce the content to feed the
+    (blog)s.",
+  "whoapply-e" => "You are willing to share your experience with Live Blog
+    with us. We want to use your story, your logo, your
+    testimonial. We want to make you famous :) Just kidding.
+    Or not.",
+  "whoapplybottominfo" => "Check, check, check, check? It's a match!
+    If that still sounds good, apply now.",
+    
+  "applynow" => "Apply now",
+  "applynowinfo" => "If you have arrived until here and still think we could work well together,
+    fill in our application form. If you still have questions, see our FAQ or get in touch.",
+    
+  "faq" => "Frequently asked questions",
+  "faqinfo" => "Here you can find some answers to key questions you might have. Any
+    other question? Need more support? Get in touch!",
+  "faq-a-q" => "Q: What are past examples of similar project you have supported?",
+  "faq-a-a" => "A: At Sourcefabric, we've worked on a range of similar projects in the past. From large media organisations using Live
+    Blog to cover breaking news or current events to informal groups of freelancers looking to tailor their global/local
+    reporting; to organisations looking for a solution for crisis reporting from the ground.",
+  "faq-b-q" => "Q: What is the time period for implementation of the project",
+  "faq-b-a" => "A: Setting up Live Blog doesn’t take more than 5 minutes. A bit more time might be needed for you to organise your
+    resources and to get training, but all in all, you can be set to go within a week or two. The length of time we work with
+    a selected project will vary, but you should expect this to be a 6 to 12 month commitment at most. If your project
+    would continue to benefit from our support and engagement we are open to extending collaboration outside of this
+    call timespan.",
+  "faq-c-q" => "Q: What kind of financial support do you offer?",
+  "faq-c-a" => "A: We have already said that there is no financial support or grants included in the <FWD:media> initiative. The
+    resources we are sharing will focus on giving you access to a great tool (Live Blog) and securing expertise. We do not
+    offer financial support or grants.Are you willing to apply for joint funding with us? Joint funding is a win-win
+    situation: both sides benefit. And we can work with you to locate and apply for the right kind of grants.",
+  
+  "bottomcontact" => "Contact us",
+  "bottomcontactinfo" => "Still have questions? Contact us at <a href='mailto:fwdmedia@sourcefabric.org'>fwdmedia@sourcefabric.org</a>",
+  "bottomcontact-title-a" => "Find us on other channels",
+  "bottomcontact-info-a" => "Visit our 
+    <a href='https://www.sourcefabric.org/' target='_blank'>website</a>
+    to learn more about Sourcefabric.
+    Follow us on 
+    <a href='http://twitter.com/sourcefabric' target='_blank'>twitter</a>
+    for our thoughts about the open
+    source, journalism and innovation.",
+  "bottomcontact-title-b" => "Join out mailing list",
+  "bottomcontact-info-b" => "We'll send you updates about our work in a monthly
+    newsletter. We won't share your email address.
+    Promise.",
+  
+/* Buttons */
+  "btn-applynow" => "Apply now",
+  "btn-more" => "Learn more",
+  "btn-checklb" => "Check out Live Blog.",
+
+/* Header */
+  "head-title" => "&lt;FWD:media&gt;",
+  "head-claim" => "Add live reporting to your site.",
+
+/* Languages */
+  "lang-en" => "en",
+  "lang-fr" => "fr",
+  "lang-es" => "es",
+
+/* Navigation */
+  "nav-menu" => "Menu",
+  "nav-home" => "Home",
+  "nav-start" => "What we offer",
+  "nav-lbabout" => "What is live blogging?",
+  "nav-what" => "What we provide",
+  "nav-who" => "Who should apply",
+  "nav-apply" => "Apply now",
+  "nav-faq" => "FAQ",
+  "nav-contact" => "Contact us"
+);
+?>
